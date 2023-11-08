6f33118bd4b25771c687cd7641e33401a1759292
smoleckk <ksmolecki123@wp.pl>
2023-11-08 21:57:32 +0100

    Add animation on scroll to main pages

--- CompanyCompanion/src/app/pages/welcome/welcome.component.ts
+++ CompanyCompanion/src/app/pages/welcome/welcome.component.ts
@@ -1,14 +1,45 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { Renderer2, ElementRef } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss'],
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements AfterViewInit  {
   hideMenu: boolean = true;
-
+  
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+  
   HideMenu() {
     this.hideMenu = !this.hideMenu;
   }
+  ngAfterViewInit() {
+    const options = {
+      root: null,
+      rootMargin: '0px',
+      threshold: 0.9,
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(this.el.nativeElement, 'animate-left');
+          this.renderer.addClass(this.el.nativeElement, 'animate-right');
+          this.renderer.addClass(this.el.nativeElement, 'drop-in-from-down');
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+
+    const elementsToObserve = document.querySelectorAll('.testimonials-col');
+    elementsToObserve.forEach((element) => {
+      observer.observe(element);
+    });
+
+    const elementsToObserve2 = document.querySelectorAll('.contact-section');
+    elementsToObserve2.forEach((element) => {
+      observer.observe(element);
+    });
+  }
 }
+
